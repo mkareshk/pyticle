5d42649d57dc9f5414b8d46b9d81d969fdefbcfb
Moein Kareshk <mkareshk@outlook.com>
2022-02-19 07:02:04 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 import setuptools
 
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     long_description = file.read()
 
 setuptools.setup(
-    name='pyticle',
-    version='0.0.3',
-    author='Moein Kareshk',
-    author_email='mkareshk@outlook.com',
-    description='A Python Library for Particle Swarm Intelligence',
+    name="pyticle",
+    version="0.0.3",
+    author="Moein Kareshk",
+    author_email="mkareshk@outlook.com",
+    description="A Python Library for Particle Swarm Intelligence",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/owhadi/pyticle',
+    long_description_content_type="text/markdown",
+    url="https://github.com/owhadi/pyticle",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    install_requires=['tabulate', 'numpy', 'imageio', 'matplotlib', 'joblib', 'pandas'],
-    python_requires='>=3.6',
+    install_requires=[
+        "tabulate>=0.8.9",
+        "imageio>=2.16.0",
+        "matplotlib>=3.5.1",
+        "joblib>=1.1.0",
+        "pandas>1.4.1",
+    ],
+    python_requires=">=3.6",
 )
